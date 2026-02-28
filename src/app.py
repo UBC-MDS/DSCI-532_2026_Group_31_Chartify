97862246b2727baaf0256a8f774d324468ce0a46
Victoria Farkas <farkasv@student.ubc.ca>
2026-02-27 22:39:00 -0800

    fix float rep

--- src/app.py
+++ src/app.py
@@ -226,7 +226,7 @@ def server(input, output, session):
         df = filtered()
         if (df["Stream"] != 0).any():
             avg_stream = df["Stream"].mean()
-            display = f"{avg_stream:,0f}"
+            display = f"{avg_stream:,.0f}"
         else:
             display = "0"
             
@@ -237,7 +237,7 @@ def server(input, output, session):
         df = filtered()
         if (df["Likes"] != 0).any():
             avg_likes = df["Likes"].mean()
-            display = f"{avg_likes:,0f}"
+            display = f"{avg_likes:,.0f}"
         else:
             display = "0"
             
